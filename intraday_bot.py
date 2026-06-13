1577fc92c08296f36e3200ca873dd31454f629fd
Hayden Hour <haydenhour@yahoo.com>
2026-06-12 21:13:14 -0400

    security: remove hardcoded credentials and implement python-dotenv environment variables

--- intraday_bot.py
+++ intraday_bot.py
@@ -7,7 +7,7 @@ import yfinance as yf
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
-from dotenv import load_load_env
+from dotenv import load_dotenv
 
 load_dotenv()
 
